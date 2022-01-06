5c17210ffdbdf31f2edf060049fdb7b05a4a75ee
mogubudu <pipkin.vladislav@gmail.com>
2022-01-06 17:10:08 +0300

    Create fetch_xkcd_comics.py

---
+++ fetch_xkcd_comics.py
@@ -0,0 +1,27 @@
+import random
+import requests
+
+
+def get_xkcd_last_comics():
+    current_commics_url = 'https://xkcd.com/info.0.json'
+    response = requests.get(current_commics_url)
+    response.raise_for_status()
+    last_comics_number = response.json()['num']
+    return last_comics_number
+
+
+def get_xkcd_random_comics():
+    last_comics_number = get_xkcd_last_comics()
+    random_number = random.randint(1, last_comics_number)
+    url = f'https://xkcd.com/{random_number}/info.0.json'
+    response = requests.get(url)
+    response = response.json()
+    return response['img'], response['alt']
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
